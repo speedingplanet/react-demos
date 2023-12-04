3c98b2088685ed57b492967a81f2c2af665745b5
John Paxton <johnpaxton@gmail.com>
2023-12-04 17:21:44 -0500

    Testing: accessing elements

--- src/testing/BasicComponent.jsx
+++ src/testing/BasicComponent.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function BasicComponent() {
-	return <div>BasicComponent</div>;
+	return (
+		<div>
+			<ul>
+				<li>Apples</li>
+				<li>Bananas</li>
+				<li>Peaches</li>
+				<li data-testid="oranges">Oranges</li>
+			</ul>
+		</div>
+	);
 }
 
 export default BasicComponent;
--- src/testing/BasicComponent.test.jsx
+++ src/testing/BasicComponent.test.jsx
@@ -1,5 +1,57 @@
-import { it, expect } from 'vitest';
+import React from 'react';
+import { it, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import BasicComponent from './BasicComponent';
 
-it('Smoke test', () => {
+it('Smoke test (it)', () => {
 	expect(1 + 1).toBe(2);
+});
+
+test('Smoke test (test)', () => {
+	expect(1 + 1).toBe(2);
+});
+
+it('should render the BasicComponent component', () => {
+	render(<BasicComponent />);
+
+	/* 
+	Find an element by its text, error/failure if not found
+	screen.getByText('Apples')
+	expect().not -> Negates the next matcher
+	toBeNull() -> Checks for nullity
+	*/
+
+	expect(screen.getByText('Apples')).not.toBeNull();
+	expect(screen.getByText('Bananas')).toBeInTheDocument();
+});
+
+it('should access the component in several ways', () => {
+	render(<BasicComponent />);
+
+	// Plain text, must be exact, case sensitive
+	expect(screen.getByText('Apples')).not.toBeNull();
+
+	// Inexact match, and case-insensitive
+	expect(screen.getByText('App', { exact: false })).not.toBeNull();
+
+	// Regular expression match (currently matching the behavior of exact)
+	expect(screen.getByText(/app/i));
+});
+
+/*
+	This may be necessary sometimes, but it goes against the philosophy of
+	testing library
+*/
+it('should allow access to the underlying DOM', () => {
+	// The nearest element wrapper around this component
+	const { container } = render(<BasicComponent />);
+
+	let secondListItem = container.querySelector('li:nth-child(2)');
+	expect(secondListItem).not.toBeNull();
+	expect(secondListItem.textContent).toEqual('Bananas');
+});
+
+it('should allow access via a test id', () => {
+	render(<BasicComponent />);
+	expect(screen.getByTestId('oranges')).not.toBeNull();
 });
